5b32c2551a209f7f28f38a134f42c0878539d6cb
kinsprite <qxqs11@126.com>
2020-06-23 09:19:42 +0800

    lazy effect run once

--- src/lazyComponent/index.tsx
+++ src/lazyComponent/index.tsx
@@ -1,29 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
-function LazyComponent() : JSX.Element {
-  const [loaded, setLoaded] = useState(false);
-  const [Component, setComponent] = useState(null as any);
+function LazyComponent(props?: {[key:string]: any}) : JSX.Element {
+  const [once] = useState(1);
+  const [result, setResult] = useState({
+    loaded: 0,
+    component: null,
+  });
 
   useEffect(() => {
     let mounted = true;
-    import('./LazyInfo').then((c) => {
-      if (mounted) {
-        setComponent(c.default);
-        setLoaded(true);
-      }
-    }, () => {
-      if (mounted) {
-        setComponent(null);
-        setLoaded(true);
-      }
-    });
+    if (once === 1) {
+      import('./LazyInfo').then((c) => {
+        if (mounted) {
+          setResult({
+            loaded: 1,
+            component: c.default,
+          });
+        }
+      }, () => {
+        if (mounted) {
+          setResult({
+            loaded: 2,
+            component: null,
+          });
+        }
+      });
+    }
 
     return () => {
       mounted = false;
     };
-  }, [loaded]);
+  }, [once]);
 
-  return (loaded && Component) || (<div>Waiting for lazy info ...</div>);
+  return result.component
+    ? React.createElement(result.component, props)
+    : (<div>Waiting for lazy info ...</div>);
 }
 
 export default LazyComponent;
