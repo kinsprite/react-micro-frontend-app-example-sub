5701d49f6450620be7290849b06556accd1a7aee
kinsprite <qxqs11@126.com>
2020-06-25 12:16:06 +0800

    typing lazy text

--- src/lazyComponent/LazyInfo.tsx
+++ src/lazyComponent/LazyInfo.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import styles from './LazyInfo.module.pcss';
 
 function LazyInfo() : JSX.Element {
-  return <div className={styles.container}>This is lazy content in App Example Sub.</div>;
+  return (
+    <div className={styles.container}>
+      <code className={styles.content}>This is lazy content in App Example Sub.</code>
+    </div>
+  );
 }
 
 export default LazyInfo;
